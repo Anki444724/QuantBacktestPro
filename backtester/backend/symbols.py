add63df76f7f9ee43f3a07f0a27308a580f77d02
Ankit Singh <your-github-ankitsingh835141@gmail.com>
2026-07-18 04:01:00 +0530

    Move nse_symbols.csv into backend

--- backtester/backend/symbols.py
+++ backtester/backend/symbols.py
@@ -1,13 +1,8 @@
 from pathlib import Path
-import os
 import pandas as pd
 
-print("CURRENT DIR:", os.getcwd())
-print("__FILE__:", __file__)
-
-CSV_FILE = (Path(__file__).parent / "../data/nse_symbols.csv").resolve()
-print("CSV PATH:", CSV_FILE)
-print("CSV EXISTS:", CSV_FILE.exists())
+CSV_FILE = Path(__file__).parent / "data" / "nse_symbols.csv"
 
 df = pd.read_csv(CSV_FILE)
+
 ALL_SYMBOLS = df.to_dict(orient="records")
